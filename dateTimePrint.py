457b9a55b79b17cc590b1c7abd6b0dd1006e416f
Duum <muhamadude@gmail.com>
2019-10-27 17:53:06 -0500

    Found the difference between times. Need to create a counter for each discrete moment of time (minutes, seconds, or milliseconds). The occurence of all discrete moments need to be added to a counter for that discrete moment

--- dateTimePrint.py
+++ dateTimePrint.py
@@ -7,6 +7,22 @@ Created on Sun Oct 27 16:42:57 2019
 
 import datetime
 
-now = datetime.now()
+first = datetime.datetime.now()
 
-print (now)
+input ("Press a button to continue")
+
+
+second = datetime.datetime.now()
+
+print ("First time was ", first)
+print ("Second time was ",  second)
+
+difference = second - first
+
+print ("Difference is ",  difference)
+
+print(first.second)
+print(second.second)
+
+ #Need to add value to counter for corresponding second of the day.
+ 
